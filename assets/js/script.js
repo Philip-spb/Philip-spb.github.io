cf2bcf8b65eeeb75ef144fcf8e9734f36819f95c
Philip-spb <philip.spb@gmail.com>
2021-04-21 02:25:57 +0300

    добавил темную тему

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,33 @@
 'use strict';
 
+function setDarkTheme() {
+    var cssFile = document.querySelector('[rel="stylesheet"]');
+    cssFile.setAttribute('href', '/assets/css/just-the-docs-dark.css');
+    localStorage.setItem('darkTheme', true);
+}
+
+function setLightTheme() {
+    var cssFile = document.querySelector('[rel="stylesheet"]');
+    cssFile.setAttribute('href', '/assets/css/just-the-docs-light.css');
+    localStorage.removeItem('darkTheme');
+}
+
 window.addEventListener('DOMContentLoaded', () => {
+    let darkTheme = localStorage.getItem('darkTheme');
+    if (darkTheme) {
+        setDarkTheme();
+    }
+    const siteTitle = document.querySelector('.site-title');
+    siteTitle.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (darkTheme) {
+            setLightTheme();
+            darkTheme = false;
+        } else {
+            setDarkTheme();
+            darkTheme = true;  
+        }
+    });
     const searchField = document.querySelector('.search-input');
     searchField.placeholder = 'Поиск...';
 });
