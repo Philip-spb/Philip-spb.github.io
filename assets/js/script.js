992e96f1f2d1df1561cf50e73780ed50e1a004c7
Philip-spb <philip.spb@gmail.com>
2021-04-21 01:12:40 +0300

    модифицировал строку поиска

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+    const searchField = document.querySelector('.search-input');
+    searchField.placeholder = 'Поиск...';
+});
