5dd951e38173887e8dc1e043b5760392e0ffa7f3
Philip-spb <philip.spb@gmail.com>
2021-04-23 14:12:08 +0300

    добавил раскрытие изображение в модальном окне

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,6 +17,7 @@ window.addEventListener('DOMContentLoaded', () => {
     if (darkTheme) {
         setDarkTheme();
     }
+
     const siteTitle = document.querySelector('.site-title');
     siteTitle.addEventListener('click', (e) => {
         e.preventDefault();
@@ -28,6 +29,58 @@ window.addEventListener('DOMContentLoaded', () => {
             darkTheme = true;
         }
     });
+
     const searchField = document.querySelector('.search-input');
     searchField.placeholder = 'Поиск...';
+
+
+    const mainContentWrap = document.querySelector('.main-content-wrap');
+
+    // Создаю модальное окно
+    const modal = document.createElement('div');
+    modal.classList.add('modal');
+    modal.style = `
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 1050;
+    display: none;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    background-color: rgba(0, 0, 0, .5)
+    `;
+
+    const modalDialog = document.createElement('div');
+    modalDialog.classList.add('modal__dialog');
+    modalDialog.style = `
+    text-align: center;
+    max-width: 90%;
+    margin: 40px auto
+    `;
+    modal.append(modalDialog);
+
+    const modalImg = document.createElement('img');
+    modalImg.style.maxWidth = "80%";
+
+    modalDialog.append(modalImg);
+
+    mainContentWrap.append(modal);
+
+    modal.addEventListener('click', () => {
+        console.log(modal.style.display);
+        modal.style.display = "none";
+    });
+
+    // Реализую открытие изображений при клике по нему
+    mainContentWrap.addEventListener('click', (e) => {
+        const mainContentWrapStyle = window.getComputedStyle(mainContentWrap);
+        if (e.target && 
+            e.target.matches(".open-modal") && 
+            +mainContentWrapStyle.width.replace(/\D/g, '') >= 800) {
+            // console.log(modal.style.display);
+            modalImg.src = e.target.src;
+            modal.style.display = "block";
+        }
+    });
 });
